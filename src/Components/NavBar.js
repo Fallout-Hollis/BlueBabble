2ed7316d4650dd9397985611c164f77955ae8158
Hollis <blueartlover6@gmail.com>
2023-06-01 20:09:53 -0600

    Created React App and Started working on Design

---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+function NavBar() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-br from-gray-900 to-gray-800">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-8 text-blue-400 leading-tight">
+          Join the BlueBabble Community
+        </h1>
+        <div className="space-x-4">
+          <button className="text-white bg-blue-400 py-3 px-6 rounded-full text-xl">
+            Login
+          </button>
+          <button className="text-white bg-blue-400 py-3 px-6 rounded-full text-xl">
+            Create Account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
